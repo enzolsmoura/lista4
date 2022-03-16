107e826a7fdbaf552302cccde6cbc53478848920
enzolsmoura <enderenzo@gmail.com>
2022-03-16 00:07:17 -0300

    todos os exercicios resolvidos

--- src/ex7.c
+++ src/ex7.c
@@ -27,6 +27,17 @@
 #define ARRAY_MAX 3 // Valor máximo para o tamanho do array. Não modifique.
 
 /* Protótipos */
+
+int produto_escalar(int array[], int array2[]){
+  int produto_escalar;
+  int i;
+  produto_escalar = 0;
+  for(i=0;i<ARRAY_MAX;i++){
+    produto_escalar += array[i]*array2[i];
+  }
+  return produto_escalar;
+}
+
 void exibe_array(int array[], int tam_array){
   int j;
   printf("[");
@@ -36,21 +47,40 @@ void exibe_array(int array[], int tam_array){
   printf("%i]",array[j]);
 }
 
-int main (int argc, char *argv[])
-{
-  int i,j,array[ARRAY_MAX][ARRAY_MAX];
-  for(i=0;i<ARRAY_MAX;i++){
-    printf("Digite a linha %i: ",i);
-    for(j=0;j<ARRAY_MAX;j++){
-      scanf("%i",&array[j][i]);
+void multiplica_array(int array[][ARRAY_MAX], int array2[][ARRAY_MAX]){
+  int i,j,soma,array_mult[ARRAY_MAX][ARRAY_MAX];
+  soma = 0;
+  for(i = 0; i<ARRAY_MAX ; i++){
+    for(j = 0; j<ARRAY_MAX; j++){
+      soma = 0;
+      soma += produto_escalar(array[i],array2[j]);
+      array_mult[i][j] = soma;
     }
   }
   printf("[");
   for(j=0;j<i-1;j++){
-    exibe_array(array[j],ARRAY_MAX);
+    exibe_array(array_mult[j],ARRAY_MAX);
     printf(",\n");
   }
-  exibe_array(array[j],ARRAY_MAX);
+  exibe_array(array_mult[j],ARRAY_MAX);
   printf("]");
+}
+    
+int main (int argc, char *argv[])
+{
+  int i,j,array[ARRAY_MAX][ARRAY_MAX], array2[ARRAY_MAX][ARRAY_MAX];
+  for(i=0;i<ARRAY_MAX;i++){
+    printf("Digite a linha %i: ",i);
+    for(j=0;j<ARRAY_MAX;j++){
+      scanf("%i",&array[i][j]);
+    }
+  }
+  for(i=0;i<ARRAY_MAX;i++){
+    printf("Digite a linha %i: ",i);
+    for(j=0;j<ARRAY_MAX;j++){
+      scanf("%i",&array2[j][i]);
+    }
+  }
+  multiplica_array(array,array2);
   return 0;
 }
